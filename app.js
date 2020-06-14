c930fc85534b04a30112336032c247319da08577
alchemistbg <alchemist_bg@abv.bg>
2020-06-14 18:38:18 +0300

    Update app.js
    Add handlers for user related routes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+const userRouter = require('./api/users');
+
 app.use(express.json());
+
+app.use('/api/user', userRouter);
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
