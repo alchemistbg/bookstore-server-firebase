4a9068c213da839cc46ca224bc6a48558cc92b9c
alchemistbg <alchemist_bg@abv.bg>
2020-06-15 10:47:12 +0300

    Update user conroller
    Implement user's register and login

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,81 @@
-module.exports = {
-    register: (req, res, next) => {
-        res.status(201).json(req.body);
-    },
+const { firebaseAdmin, firestore, firebase } = require('../database/database');
 
-    login: (req, res, next) => {
-        res.status(200).json({
-            ...req.body
-        });
-    }
+module.exports = {
+	register: (req, res, next) => {
+		const newUser = { ...req.body };
+		let userId, token;
+		firestore.doc(`/users/${newUser.username}`).get()
+			.then((user) => {
+				if (user.exists) {
+					const error = new Error("Invalid data");
+					error.status = 400;
+					error.code = "auth/username-already-in-use";
+					error.message = "The username is already in use!";
+					throw error;
+					// next(error);
+					// return;
+				} else {
+					return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+				}
+			})
+			.then((userData) => {
+				userId = userData.user.uid;
+				return userData.user.getIdToken();
+			})
+			.then((idToken) => {
+				token = idToken;
+				const newUserData = {
+					userId: userId,
+					// personalInfo: {
+					//     firstname: newUser.firstname,
+					//     lastname: newUser.lastname,
+					// },
+					firstname: newUser.firstname,
+					lastname: newUser.lastname,
+					username: newUser.username,
+					email: newUser.email,
+					// imageUrl: 'image',
+					role: "user",
+					registeredAt: new Date().toISOString()
+				}
+				return firestore.doc(`/users/${newUser.username}`).set(newUserData);
+			})
+			.then((arguments) => {
+				console.log(arguments);
+				res
+					.status(201)
+					.json({
+						token
+					});
+			})
+			.catch((fbError) => {
+				fbError.status = 400;
+				next(fbError);
+			});
+	},
+
+	login: (req, res, next) => {
+		const user = { ...req.body }
+		firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+			.then((userData) => {
+				return userData.user.getIdToken();
+			})
+			.then((token) => {
+				res
+					.status(200)
+					.json({
+						token
+					});
+			})
+			.catch((fbError) => {
+				fbError.status = 400;
+				next(fbError);
+			});
+	},
+
+	getProfile: (req, res, next) => {
+		const userData = {};
+	}
+
+
 };
