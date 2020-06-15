e4f595c6fa21913a0c5169d7cafe52cdbf8fa510
alchemistbg <alchemist_bg@abv.bg>
2020-06-15 09:58:27 +0300

    Update app.js
    Add books related routes
    
    Add global error handler

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 
 const userRouter = require('./api/users');
+const bookRouter = require('./api/books');
 
 app.use(express.json());
 
 app.use('/api/user', userRouter);
+app.use('/api/book', bookRouter);
+
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.status || 500;
+	const { code, message } = error;
+	res.status(status).json({ code, message });
+	next();
+})
 
 app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+	console.log('App listening on port 3000!');
 });
