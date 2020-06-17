f657a0e23955460dd79ee63d6b93bc4cd9899361
alchemistbg <alchemist_bg@abv.bg>
2020-06-17 11:22:44 +0300

    Implement the method for adding a book to a database if the user is admin

--- controllers/books.js
+++ controllers/books.js
@@ -4,7 +4,35 @@ module.exports = {
 	},
 
 	addBook: (req, res, next) => {
-		res.send("Add book!");
+		// console.log(req.user)
+		if (req.user.role === 'admin') {
+			const newBook = { ...req.body };
+			firestore.doc(`/books/${newBook.isbn}`).get()
+				.then((book) => {
+					if (book.exists) {
+						const error = new Error("Invalid data");
+						error.status = 400;
+						error.code = "book/isbn-already-in-use";
+						error.message = "Book with this ISBN already exists!";
+						throw error;
+					} else {
+						return firestore.doc(`/books/${newBook.isbn}`).set(newBook)
+					}
+				})
+				.then(() => {
+					res.status(201).json({
+						message: 'OK'
+					});
+				})
+				.catch((fbError) => {
+					fbError.status = fbError.status || 400;
+					next(fbError);
+				});
+		} else {
+			return res.status(403).json({
+				message: "Unauthorized"
+			});
+		}
 	},
 
 	getBook: (req, res, next) => {
