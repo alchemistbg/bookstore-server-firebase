6a67b9c4246933d6d687385c18fbe93fea08a1cf
alchemistbg <alchemist_bg@abv.bg>
2020-07-02 17:34:38 +0300

    Clear comments

--- utils/auth.js
+++ utils/auth.js
@@ -4,7 +4,6 @@ module.exports = (req, res, next) => {
 	let token;
 	if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
 		token = req.headers.authorization.split("Bearer ")[1];
-		// console.log("Auth file: ", token);
 	} else {
 		const error = new Error("Invalid data");
 		error.status = 403;
@@ -16,24 +15,17 @@ module.exports = (req, res, next) => {
 	firebaseAdmin.auth().verifyIdToken(token)
 		.then((decodedToken) => {
 			req.user = {};
-			// req.user = { ...decodedToken };
-			// return firestore.collection("users").where("userId", "==", req.user.uid).limit(1).get();
 			return firestore.collection("users").where("userId", "==", decodedToken.uid).limit(1).get();
 		})
 		.then((data) => {
 			req.user = data.docs[0].data();
-			// console.log(req.user.username);
 			return next();
 		})
 		.catch((fbError) => {
-			// console.log(fbError);
-			// next(fbError);
-			// error.fbError = 403;
 			const error = new Error("Invalid data");
 			error.status = 403;
 			error.code = "auth/argument-error";
 			error.message = "Error while verifying token";
 			next(error);
-			// throw error;
 		});
 }
