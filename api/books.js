236b6a5bcf22ff020a689d88708f245daf52ead9
alchemistbg <alchemist_bg@abv.bg>
2020-10-08 17:30:06 +0200

    Add route for deleting comment from
    It is intended to be used by admin
    
    Small code reorganization

--- api/books.js
+++ api/books.js
@@ -13,15 +13,17 @@ router.patch('/:isbn', auth, booksController.updateBook);
 router.delete('/:isbn', auth, booksController.deleteBook);
 
 // CRUD operations on comments
-router.get('/:isbn/comments', booksController.getComments);
 router.post('/:isbn/comments', auth, booksController.postComment);
+router.get('/:isbn/comments', booksController.getComments);
+router.delete('/:isbn/comment/:commentId', auth, booksController.deleteComments);
 
-// CRUD operations on favorites - TODO
+// CRUD operations on favorites - TODO?
 // router.post('/:isbn/favorites', booksController.likeBook);
 
 // CRUD operations on likes
-router.get('/:isbn/likes', auth, booksController.getLikes);
 router.post('/:isbn/likes', auth, booksController.postLike);
+// This route shows books liked by user, so it should be moved in user-related routes?!
+router.get('/:isbn/likes', auth, booksController.getLikes);
 router.delete('/:isbn/likes', auth, booksController.deleteLike);
 
 module.exports = router;
