56345b6e326ae4bccb1c928242c06240117ce825
alchemistbg <alchemist_bg@abv.bg>
2020-10-10 16:48:27 +0200

    Create error messages object for comments-related errors

---
+++ utils/errors/commentErrors.js
@@ -0,0 +1,18 @@
+// const errorName = 'Comment error';
+
+module.exports = {
+
+    // template: {
+    //     name: '',
+    //     status: 0,
+    //     code: '',
+    //     message:''
+    // },
+
+    commentNotFound: {
+        name: 'Comment error',
+        status: 404,
+        code: 'comment/comment-not-found',
+        message: 'Comment not found!'
+    }
+}
