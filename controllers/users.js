92b9db9475aecbe3255613a4e954cf2753c7e69a
alchemistbg <alchemist_bg@abv.bg>
2020-10-12 18:47:55 +0200

    Update getProfile function
    Implement reading of user profile

--- controllers/users.js
+++ controllers/users.js
@@ -114,16 +114,46 @@ module.exports = {
 	},
 
 	getProfile: (req, res, next) => {
-		// const userData = {};
-		// console.log(req.user.username);
-		// console.log(req.params.username);
-		if (req.user.username != req.params.username) {
-			res.json({
-				message: "Public profile"
+		if ((Object.keys(req.user).length === 0) || (req.user.username !== req.params.username)) {
+			firestore.doc(`users/${req.params.username}`).get()
+				.then((userSnapshot) => {
+					const { role, registeredAt, userId, ...userData } = userSnapshot.data();
+					return res.status(200).json({
+						message: "Public profile",
+						userData
+					});
+				})
+				.catch((fsError) => {
+
 			});
 		} else {
-			res.json({
-				message: "Private profile"
+			const commentsOrderDirection = req.query.commentsDort || "desc";
+			const likesOrderDirection = req.query.likesDort || "desc";
+
+			const userProfile = {
+				...req.user,
+				comments: [],
+				likes: []
+			};
+
+			firestore.collection(`comments`).where('username', '==', req.user.username).orderBy('createdAt', commentsOrderDirection).get()
+				.then((commentsQuery) => {
+					commentsQuery.forEach((comment) => {
+						userProfile.comments.push(comment.data());
+					});
+					return firestore.collection("likes").where("username", "==", req.user.username).orderBy("createdAt", likesOrderDirection).get();
+				})
+				.then((likesQuery) => {
+					likesQuery.forEach((like) => {
+						userProfile.likes.push(like.data());
+					});
+					res.status(200).json({
+						message: "Private profile",
+						userProfile
+					});
+				})
+				.catch((fsError) => {
+					console.log(fsError);
 			});
 		}
 	},
