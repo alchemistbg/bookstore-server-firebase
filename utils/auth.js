066f75544badf9ba4593abc799556b8c0377b57e
alchemistbg <alchemist_bg@abv.bg>
2020-10-13 20:58:10 +0200

    Update auth file
    Now it uses error messages

--- utils/auth.js
+++ utils/auth.js
@@ -1,31 +1,29 @@
 const { firebaseAdmin, firestore } = require('../database/database')
+const error = require('./errors/error');
+const userMessages = require('../utils/errors/userErrors');
 
 module.exports = (req, res, next) => {
 	let token;
+	req.user = {};
+
 	if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
 		token = req.headers.authorization.split("Bearer ")[1];
+		firebaseAdmin.auth().verifyIdToken(token)
+			.then((decodedToken) => {
+				return firestore.collection("users").where("userId", "==", decodedToken.uid).limit(1).get();
+			})
+			.then((data) => {
+				req.user = data.docs[0].data();
+				return next();
+			})
+			.catch((fbError) => {
+				// console.log(fbError);
+				next(new error(userMessages.userInvalidToken));
+			});
 	} else {
-		const error = new Error("Invalid data");
-		error.status = 403;
-		error.code = "auth/unauthorized";
-		error.message = "Unauthorized";
-		throw error;
+		// token = "empty";
+		// next(new error(userMessages.unauthorized));
+		return next();
 	}
 
-	firebaseAdmin.auth().verifyIdToken(token)
-		.then((decodedToken) => {
-			req.user = {};
-			return firestore.collection("users").where("userId", "==", decodedToken.uid).limit(1).get();
-		})
-		.then((data) => {
-			req.user = data.docs[0].data();
-			return next();
-		})
-		.catch((fbError) => {
-			const error = new Error("Invalid data");
-			error.status = 403;
-			error.code = "auth/argument-error";
-			error.message = "Error while verifying token";
-			next(error);
-		});
 }
