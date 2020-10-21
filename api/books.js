86674b99796d7da789359682d8b08646fc55c376
alchemistbg <alchemist_bg@abv.bg>
2020-10-21 15:37:50 +0200

    Update 'books' api to use new organization of controllers

--- api/books.js
+++ api/books.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const booksController = require('../controllers/books');
+const commentsController = require('../controllers/comments');
+const likesController = require('../controllers/likes');
 const auth = require('../utils/auth');
 
 router.get('/', booksController.getAllBooks);
@@ -13,17 +15,17 @@ router.patch('/:isbn', auth, booksController.updateBook);
 router.delete('/:isbn', auth, booksController.deleteBook);
 
 // CRUD operations on comments
-router.post('/:isbn/comments', auth, booksController.postComment);
-router.get('/:isbn/comments', booksController.getComments);
-router.delete('/:isbn/comment/:commentId', auth, booksController.deleteComments);
+router.post('/:isbn/comments', auth, commentsController.postComment);
+router.get('/:isbn/comments', commentsController.getComments);
+router.delete('/:isbn/comment/:commentId', auth, commentsController.deleteComment);
 
 // CRUD operations on favorites - TODO?
 // router.post('/:isbn/favorites', booksController.likeBook);
 
 // CRUD operations on likes
-router.post('/:isbn/likes', auth, booksController.postLike);
+router.post('/:isbn/likes', auth, likesController.postLike);
 // This route shows books liked by user, so it should be moved in user-related routes?!
-router.get('/:isbn/likes', auth, booksController.getLikes);
-router.delete('/:isbn/likes', auth, booksController.deleteLike);
+router.get('/:isbn/likes', auth, likesController.getBookLikes);
+router.delete('/:isbn/likes', auth, likesController.deleteLike);
 
 module.exports = router;
