402eae8ea2311f88e15aa8b1f0ff7833e76ca8e2
alchemistbg <alchemist_bg@abv.bg>
2020-10-21 15:38:56 +0200

    Update '-users' api to use new organization of controllers
    Add new ednpoints for getting user's likes and comments

--- api/users.js
+++ api/users.js
@@ -7,9 +7,12 @@ const userController = require('../controllers/users');
 const auth = require('../utils/auth');
 
 router.post('/register', validator.registrationDataValidator(), userController.register);
-router.post('/login', userController.login);
+router.post('/login', validator.loginDataValidator(), userController.login);
 
 router.get('/:username', auth, userController.getProfile);
 router.patch('/:username', auth, userController.updateProfile);
+
+router.get('/:username/comments', auth, userController.getUserComments);
+router.get('/:username/likes', auth, userController.getUserLikes);
 
 module.exports = router;
